01fc6773565190303b26601e233c4b7614cbedc5
Ian D'Souza <dsouza.ianp@gmail.com>
2022-11-13 01:57:14 -0500

    god blessed us

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import TypewriterComponent from "typewriter-effect";
-import './App.css';
+import "./App.css";
+
 
 function App() {
   const data = require("./words_dictionary.json");
   let wordArr = Object.keys(data);
-  let easyWords = wordArr.filter(x => x.length <= 5);
-  let mediumWords = wordArr.filter(x => x.length <= 8)
-  let hardWords = wordArr.filter(x => x.length >= 10);
+  let easyWords = wordArr.filter((x) => x.length <= 5);
+  let mediumWords = wordArr.filter((x) => x.length <= 8);
+  let hardWords = wordArr.filter((x) => x.length >= 10);
   let inputArr = new Array(50).fill("");
   let pause = 1000;
   let ezWords = selectWords(easyWords);
   let words = populateArray(ezWords);
-  let tempString = arrToString(ezWords);
+  const [tempString, setTempString] = useState(arrToString(ezWords));
+  const [index, setIndex] = useState(0);
 
   function selectWords(wordArr) {
     let returnArr = [];
@@ -27,7 +29,9 @@ function App() {
     return returnArr;
   }
 
-  let index = 0;
+ 
+
+
 
   function populateArray(arr) {
     return arr.map(wordObj);
@@ -39,52 +43,48 @@ function App() {
 
   function handleKeyPress(e) {
     let key = e.key;
-      if (key === "Enter"){
+    if (key === "Enter") {
+      return;
+    }
+    let obj = words[index];
+    let corKey = obj.word.charAt(obj.k);
+    if (key === " " && obj.k >= 0) {
+      document.getElementById("form1").reset();
+      if (obj.k === 0) {
         return;
       }
-      let obj = words[index];
-      let corKey = obj.word.charAt(obj.k);
-      if (key === " " && obj.k >= 0) {
-        document.getElementById('form1').reset();
-        if (obj.k === 0) {
-          return;
-        }
-        if (valUntilK(obj.k, index) && inputArr[index].length ===words[index].word){ // if true, whole word has been completed correctly
-
-        }
-        else{
-
-        }
-        ++index;
-        return;
+      if (
+        valUntilK(obj.k, index) &&
+        inputArr[index].length === words[index].word
+      ) {
+        // if true, whole word has been completed correctly
+      } else {
       }
-      inputArr[index] = inputArr[index].concat(key)
-      console.log(key);
-      console.log(valUntilK(obj.k, index));
-      if (valUntilK(obj.k, index)){ // if true, word up until this point is correct
-
-      }
-      else{
-
-      }
-      console.log("keyPress: " + index);
-      console.log(inputArr);
-      obj.k += 1;
-
+      setIndex(index+1);
+      return;
+    }
+    inputArr[index] = inputArr[index].concat(key);
+    console.log(key);
+    console.log(valUntilK(obj.k, index));
+    if (valUntilK(obj.k, index)) {
+      // if true, word up until this point is correct
+    } else {
+    }
+    console.log("keyPress: " + index);
+    console.log(inputArr);
+    obj.k += 1;
   }
 
   function handleKeyDown(e) {
     let obj = words[index];
-    if (e.key === 'Backspace' && obj.k > 0) {
+    if (e.key === "Backspace" && obj.k > 0) {
       --obj.k;
       inputArr[index] = inputArr[index].slice(0, -1);
-      if (valUntilK(obj.k, index)){ // if true, word up until this point is correct
-
+      if (valUntilK(obj.k, index)) {
+        // if true, word up until this point is correct
+      } else {
       }
-      else{
-
-      }
-      console.log("keyDown: " + index)
+      console.log("keyDown: " + index);
       console.log(inputArr);
     }
   }
@@ -92,7 +92,6 @@ function App() {
   function arrToString(arr) {
     return arr.join(" ");
   }
-
 
   function valUntilK(k, index) {
     let returnVal = true;
@@ -104,27 +103,72 @@ function App() {
     return returnVal;
   }
 
+  function resetValues() {
+  let inputArr = new Array(50).fill("");
+  let ezWords = selectWords(easyWords);
+  let words = populateArray(ezWords);
+  setTempString(arrToString(ezWords));
+  setIndex(0);
+  }
+
   return (
     <div>
-      <h1><TypewriterComponent
-        onInit={(typewriter) =>
-          typewriter.typeString("finkytpye").start().pauseFor(pause).deleteAll()
-            .typeString('flimsytype').start().pauseFor(pause).deleteAll()
-            .typeString('flimdtype').start().pauseFor(pause).deleteAll()
-            .typeString('flankytype').start().pauseFor(pause).deleteAll()
-            .typeString('flirtytype').start().pauseFor(pause).deleteAll()
-            .typeString('fliarsytype').start().pauseFor(pause).deleteAll()
-            .typeString('flaetytype').start().pauseFor(pause).deleteAll()
-            .typeString('flintytype').start().pauseFor(pause).deleteAll()
-            .typeString('flype').start().pauseFor(pause).deleteAll()
-            .typeString('funkytype').start()
-        }
-      />
+      <h1>
+        <TypewriterComponent
+          onInit={(typewriter) =>
+            typewriter
+              .typeString("finkytpye")
+              .start()
+              .pauseFor(pause)
+              .deleteAll()
+              .typeString("flimsytype")
+              .start()
+              .pauseFor(pause)
+              .deleteAll()
+              .typeString("flimdtype")
+              .start()
+              .pauseFor(pause)
+              .deleteAll()
+              .typeString("flankytype")
+              .start()
+              .pauseFor(pause)
+              .deleteAll()
+              .typeString("flirtytype")
+              .start()
+              .pauseFor(pause)
+              .deleteAll()
+              .typeString("fliarsytype")
+              .start()
+              .pauseFor(pause)
+              .deleteAll()
+              .typeString("flaetytype")
+              .start()
+              .pauseFor(pause)
+              .deleteAll()
+              .typeString("flintytype")
+              .start()
+              .pauseFor(pause)
+              .deleteAll()
+              .typeString("flype")
+              .start()
+              .pauseFor(pause)
+              .deleteAll()
+              .typeString("funkytype")
+              .start()
+          }
+        />
       </h1>
       <div>
         <p>{tempString}</p>
+        <p>{index}</p>
         <form id="form1">
-        <input className="text-box" type="text" onKeyPress={(e) => handleKeyPress(e)} onKeyDown={(e) => handleKeyDown(e)} autoFocus></input>
+          <input
+            className="text-box"
+            type="text"
+            onKeyPress={(e) => handleKeyPress(e)}
+            onKeyDown={(e) => handleKeyDown(e)}
+            autoFocus
+          ></input>
         </form>
       </div>
       <div>
@@ -133,8 +177,14 @@ function App() {
       <div>
         <p className="acc-wpm">{"ACC: " + " | " + "   " + "WPM: "}</p>
       </div>
+      <div>
+      <button className="restart_btn"
+      onClick={(e)=>resetValues()}>
+      Restart
+    </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
