f25156a3c7a4ef4835d89be75dafb6bcaf3ffac1
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2022-11-12 19:55:29 -0500

    app functionality

--- src/App.js
+++ src/App.js
@@ -5,34 +5,61 @@ import TypewriterComponent from "typewriter-effect";
 function App() {
   const data = require("./words_dictionary.json");
   let wordArr = Object.keys(data);
-  let easyWords = wordArr.filter(x => x.length<=5);
-  let mediumWords = wordArr.filter(x => x.length<=8)
-  let hardWords = wordArr.filter(x => x.length>=10);
-  
-  function selectWords(wordArr){
-      let returnArr =[];
-      let indTracker = new  Set();
-      while (returnArr.length<50){
-          let indVal = Math.floor(Math.random()*wordArr.length);
-          if (!indTracker.has(indVal)){
-              returnArr.push(wordArr[indVal]);
-          }
-          indTracker.add(indVal);
+  let easyWords = wordArr.filter(x => x.length <= 5);
+  let mediumWords = wordArr.filter(x => x.length <= 8)
+  let hardWords = wordArr.filter(x => x.length >= 10);
+
+  function selectWords(wordArr) {
+    let returnArr = [];
+    let indTracker = new Set();
+    while (returnArr.length < 50) {
+      let indVal = Math.floor(Math.random() * wordArr.length);
+      if (!indTracker.has(indVal)) {
+        returnArr.push(wordArr[indVal]);
       }
-      return returnArr;
-  }  
-  
-  function arrToString(arr){
-      return arr.join(" ");
+      indTracker.add(indVal);
+    }
+    return returnArr;
   }
 
-  let tempString= arrToString(selectWords(easyWords));
+  let index = 0;
+
+  function handleKeyPress(e) {
+    let key = e.key;
+    let corKey = tempString.charAt(index);
+    if (key === " " && corKey !== " ") {
+      return;
+    }
+    console.log(key);
+    console.log(corKey === key);
+    console.log("keyPress: " + index);
+    index = index+1;
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === 'Backspace' && index > 0) {
+      index = index-1;
+      console.log("keyDown: " + index)
+    }
+  }
+
+  function arrToString(arr) {
+    return arr.join(" ");
+  }
+
+  let tempString = arrToString(selectWords(easyWords));
 
   return (
     <div>
+      <h1><TypewriterComponent
+        onInit={(typewriter) =>
+          typewriter.typeString("funkytype").start()
+        }
+      />
+      </h1>
       <p>{tempString}</p>
-      <textarea></textarea>
-      <TypewriterComponent/>
+      <p>{"" + index + "/" + 50}</p>
+      <input type="text" onKeyPress={(e) => handleKeyPress(e)} onKeyDown={(e) => handleKeyDown(e)}/>
     </div>
   )
 }
